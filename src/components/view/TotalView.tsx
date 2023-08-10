39d61bf63dcb59f84da2badbbe49d4cd8cff57ba
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2023-08-10 22:08:35 +0200

    Modificación index.css y TotalView

--- src/components/view/TotalView.tsx
+++ src/components/view/TotalView.tsx
@@ -2,12 +2,21 @@ import { Box, Button } from "@mui/material";
 import { FC } from "react";
 
 interface Props {
-    total: number;
+  total: number;
 }
-export const TotalView:FC<Props> = ({total}) => {
+export const TotalView: FC<Props> = ({ total }) => {
   return (
-    <Box sx={{marginTop: "20px",display: "flex", alignItems: "center", justifyContent: "flex-end !important"}}>
-        <Button variant="contained" color="success"  >{total}</Button>
+    <Box
+      sx={{
+        marginTop: "20px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-end",
+      }}
+    >
+      <Button variant="contained" color="success">
+        {total}
+      </Button>
     </Box>
-  )
-}
+  );
+};
