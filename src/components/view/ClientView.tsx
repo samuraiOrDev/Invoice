812790f5009622a650b1a45c72df5930eb0ac755
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2023-08-10 22:12:21 +0200

    End 10-08-2023 - 22:12

--- src/components/view/ClientView.tsx
+++ src/components/view/ClientView.tsx
@@ -2,28 +2,30 @@ import { Box, Typography, List, ListItem, ListItemText } from "@mui/material";
 import { FC } from "react";
 
 interface TypeClient {
-    name: string;
-    lastName: string;
-    address: {
-        country: string;
-        city: string;
-        street: string;
-        number: number;
-    }
+  name: string;
+  lastName: string;
+  address: {
+    country: string;
+    city: string;
+    street: string;
+    number: number;
+  };
 }
 interface Props {
-    client: TypeClient;
+  client: TypeClient;
 }
-export const ClientView:FC<Props> = ({
-    client
-}) => {
-    const { name, lastName, address } = client;
-    const { country, street, number, city} = address;
+export const ClientView: FC<Props> = ({ client }) => {
+  const { name, lastName, address } = client;
+  const { country, street, number, city } = address;
   return (
     <Box sx={{ width: "50%" }}>
-      <Typography variant="h3" sx={{marginBottom: "5px", color: "#efb810"}}>Datos del cliente</Typography>
-      <List sx={{ border: "3px solid #efb810", borderRadius: "8px", color: "#fff" }}>
-        <ListItem>
+      <Typography variant="h3" sx={{ marginBottom: "5px", color: "#efb810" }}>
+        Datos del cliente
+      </Typography>
+      <List
+        sx={{ border: "3px solid #efb810", borderRadius: "8px", color: "#fff" }}
+      >
+        <ListItem sx={{ bgcolor: "#333", color: "#efb810" }}>
           <ListItemText primary={`${name} ${lastName}`} />
         </ListItem>
         <ListItem sx={{ borderTop: "3px solid #efb810" }}>
--- src/components/view/CompanyView.tsx
+++ src/components/view/CompanyView.tsx
@@ -2,19 +2,23 @@ import { Box, Typography, List, ListItem, ListItemText } from "@mui/material";
 import { FC } from "react";
 
 interface TypeCompany {
-    name: string;
-    fiscalNumber: number;
+  name: string;
+  fiscalNumber: number;
 }
 interface Props {
-    company: TypeCompany
+  company: TypeCompany;
 }
 export const CompanyView: FC<Props> = ({ company }) => {
-    const { name, fiscalNumber} = company;
+  const { name, fiscalNumber } = company;
   return (
     <Box sx={{ width: "50%" }}>
-      <Typography variant="h3" sx={{color: "#efb810", marginBottom: "5px"}}>Datos de la empresa</Typography>
-      <List sx={{ border: "3px solid #efb810", borderRadius: "8px", color: "#fff" }}>
-        <ListItem>
+      <Typography variant="h3" sx={{ color: "#efb810", marginBottom: "5px" }}>
+        Datos de la empresa
+      </Typography>
+      <List
+        sx={{ border: "3px solid #efb810", borderRadius: "8px", color: "#fff" }}
+      >
+        <ListItem sx={{ bgcolor: "#333", color: "#efb810" }}>
           <ListItemText primary={name} />
         </ListItem>
         <ListItem sx={{ borderTop: "3px solid #efb810" }}>
